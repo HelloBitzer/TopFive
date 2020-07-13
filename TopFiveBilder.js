0a7e73a8a526d13246274a2bcf98a7a78750a52d
Nathalie Bitzer <68108453+HelloBitzer@users.noreply.github.com>
2020-07-13 14:03:12 +0200

    Add files via upload

---
+++ TopFiveBilder.js
@@ -0,0 +1,22 @@
+// JavaScript source code
+
+// liste der Bilder
+
+var bilder = [
+    { src: "bilder/vaughan_flood.jpg", alt: "Texas Flood, by Stevie Ray Vaughan" },
+    { src: "bilder/clapton_cream.jpg", alt: "The Cream of Clapton, by Eric Clapton" },
+    { src: "bilder/mayer_try.jpg", alt: "Try! Live, by John Mayer Trio" },
+    { src: "bilder/waters_live.jpg", alt: "Live, by Muddy Waters" },
+    { src: "bilder/house_delta.jpg", alt: "Original Delta Blues, by Son House" },
+
+    { src: "bilder/cream_live.jpg", alt: "Royal Albert Hall, by Cream" },
+    { src: "bilder/shepherd_ledbetter.jpg", alt: "Ledbetter Heights, by Kenny Wayne Shepherd" },
+    { src: "bilder/johnson_complete.jpg", alt: "The Complete Recordings, by Robert Johnson" },
+    { src: "ilder/hendrix_experience.jpg", alt: "Are You Experienced, by Jimmi Hendrix" },
+    { src: "bilder/jorma_blues.jpg", alt: "Blue Country Heart, by Jorma Kaukonen" },
+    { src: "bilder/johnson_wizard.jpg", alt: "The Original Guitar  Wizard, by Lonnie Johnson" },
+    { src: "bilder/lang_lie.jpg", alt: "Lie to Me, by Jonny Lang " }
+   
+];
+    
+ 
